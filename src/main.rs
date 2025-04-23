9c322adbae27d1e8270558c21e217afce11a6301
CHAMPES <nathan.champes@gmail.com>
2025-04-23 12:31:35 +0200

    Pour toi nico

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,6 @@ fn main() {
     let author1 = Author::new("Victor Hugo".to_string());
     println!("test display auteur : {}", author1);
 
-    // Je découvre juste l'utilisation des trucs
-    let mut vec = Vec::new();
-    vec.push(author1.id);
-    let book1 = Book::new("Les misérables".to_string(), vec);
+    let book1 = Book::new("Les misérables".to_string(), vec![author1.id]);
     println!("test display livre : {}", book1);
 }
